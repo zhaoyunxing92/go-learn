067e90fa1b30ef38c5bc89bcc964b05166856bfc
zhaoyunxing <zhaoyunxing@apache.org>
2023-03-01 19:43:47 +0800

    添加日志

--- zap/log/log.go
+++ zap/log/log.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
 	"time"
 )
 
 func main() {
-	logger, _ := zap.NewProduction()
+	encoder := zapcore.NewConsoleEncoder(
+		zapcore.EncoderConfig{
+			MessageKey:     "msg",
+			LevelKey:       "level",
+			TimeKey:        "time",
+			CallerKey:      "line",
+			NameKey:        "logger",
+			FunctionKey:    "func",
+			StacktraceKey:  "stacktrace",
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.0000"),
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+		})
+
+	zap.NewDevelopmentConfig()
+	core := zapcore.NewCore(encoder, os.Stdout, zap.DebugLevel)
+	logger := zap.New(core)
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
 	sugar.Infow("failed to fetch URL",
 		// Structured context as loosely typed key-value pairs.
-		"url", "hello",
+		"url", "https://github.com",
 		"attempt", 3,
 		"backoff", time.Second,
 	)
